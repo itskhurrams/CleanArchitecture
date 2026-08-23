f058833defe35620f476915f00cdc5367982818a
Khurram <itskhurrams@gmail.com>
2026-08-23 21:25:03 +0500

    Domain Changes

--- Clean.Architecture.Domain/Interfaces/Defination/IMeritalStatusData.cs
+++ Clean.Architecture.Domain/Interfaces/Defination/IMeritalStatusData.cs
@@ -1,8 +1,8 @@
-﻿using Clean.Architecture.Domain.Defination;
+using Clean.Architecture.Domain.Defination;
 
 using System.Collections.Generic;
 
-namespace Clean.Architecture.Application.Domain.Defination {
+namespace Clean.Architecture.Domain.Interfaces.Defination {
     public interface IMeritalStatusData {
         IEnumerable<MeritalStatus> GetMeritalStatuses();
         MeritalStatus GetMeritalStatusById(short Id);
